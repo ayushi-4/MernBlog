cdeae4ff8139878f58c7fcfeacd89110237d229e
ayushi-4 <goyalayushi11100@gmail.com>
2024-05-30 14:20:57 +0530

    connect to the databse

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
-
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(
+    () =>{
+        console.log("database is connected");
+    }
+).catch((error) =>{
+    console.log(error);
+});
 const app = express();
 
 app.listen(3000,()=>{
