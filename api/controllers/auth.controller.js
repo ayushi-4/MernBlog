6d7eb1664c3ade93758eb7a939fcf32211a1800c
ayushi-4 <goyalayushi11100@gmail.com>
2024-05-30 16:47:13 +0530

    add a middleware and a function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async(req,res) =>{
+export const signup = async(req,res,next) =>{
     const { username , email, password} = req.body;
 
     if(!username || !email || !password  || username === '' || email === '' || password=== '' ){
@@ -17,7 +17,7 @@ export const signup = async(req,res) =>{
     await newUser.save();
     res.json("signup successfu");
   } catch (error) {
-    res.status(500).json({message:error.message})
+    next(error);
   }
     
 }
---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+import { error } from "console";
+export const errorHandler = (statusCode , message) =>{
+    const error = new Error()
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
