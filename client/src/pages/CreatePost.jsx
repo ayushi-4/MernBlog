fe6246d81df8bf2dcb2cfcb2d9be26b0c4bc4df9
ayushi-4 <goyalayushi11100@gmail.com>
2024-06-02 02:55:34 +0530

    publishData

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,11 +5,14 @@ import { useState } from 'react';
 import {getDownloadURL, getStorage, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase.js';
 import { ref } from 'firebase/storage';
+import {useNavigate} from 'react-router-dom';
 export default function CreatePost() {
     const[file , setFile] = useState(null);
     const[imageUploadProgress , setImageUploadProgress] = useState(null);
     const[imageUploadError , setImageUploadError] = useState(null);
     const [formData , setFormData] = useState ({});
+    const [publishError , setpublishError] = useState (null);
+    const navigate = useNavigate();
     const handleUploadImage = async() =>{
         try {
             if(!file){
@@ -45,16 +48,48 @@ export default function CreatePost() {
             console.log(error);
         }
     };
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create' ,{
+                method: 'POST',
+                headers:{
+                    'Content-Type' : 'application/json' ,
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if(!res.ok){
+                setpublishError(data.message);
+                return;
+            }
+            
+            if(res.ok){
+
+                setpublishError(null);
+                navigate(`/post/${data.slug}`);
+            }
+        } catch (error) {
+            setpublishError('something went wrong');
+        }
+    };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
 
     <h1 className='text-center text-3xl my-7 font-semibold'><b>Create a Post</b></h1>
-<form className='flex flex-col gap-4'>
+<form className='flex flex-col gap-4' onSubmit={handleSubmit}>
 <div className='flex flex-col gap-4 sm:flex-row justify-between'>
 
     <TextInput type='text' placeholder='Title' required id='title'
-    className='flex-1'/>
-    <select>
+    className='flex-1' 
+    onChange={(e) =>
+        setFormData({...formData, title: e.target.value})
+    }/>
+    <select 
+    onChange={(e) =>
+         setFormData({...formData , category: e.target.value})
+    }
+    >
         <option value ="uncategorized">Select a Category</option>
         <option value ="Cosmetics">Cosmetics</option>
         <option value ="Sports">Sports</option>
@@ -72,8 +107,14 @@ export default function CreatePost() {
         alt='upload'
         className='w-full h-72 object-cover'/>
     )}
-    <ReactQuill theme="snow" placeholder='Write something...' className='h-72 mb-12' required />
+    <ReactQuill theme="snow" placeholder='Write something...' className='h-72 mb-12' required
+    
+    onChange={(value) =>{
+        setFormData({...formData,content: value});
+    }}
+    />
     <Button type='submit' gradientDuoTone='purpleToBlue'>Publish </Button>
+    {publishError && <Alert className = 'mt-5' color='failure'>{publishError} </Alert>}
 </form>
     </div>
   )
