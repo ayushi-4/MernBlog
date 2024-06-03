f22391ca4ac74084f6797da43d44f855d2e9f4eb
ayushi-4 <goyalayushi11100@gmail.com>
2024-06-03 21:22:02 +0530

    delete comment

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { Textarea } from 'flowbite-react';
 import {FaThumbsUp} from 'react-icons/fa';
 import { Button } from 'flowbite-react';
-export default function Comment({comment , onLike , onEdit }) {
+export default function Comment({comment , onLike , onEdit , onDelete}) {
     const[user,setUser] = useState({});
     const[isEditing , setIsEditing] = useState(false);
     const[editedContent , setEditedContent] = useState(comment.content);
@@ -111,11 +111,19 @@ export default function Comment({comment , onLike , onEdit }) {
             </p>
      {
       currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-        <button type='button' 
+      <>
+
+<button type='button' 
         onClick={handleEdit}
         className='text-gray-400 hover:text-blue-500'>
             Edit
         </button>
+        <button type='button' 
+        onClick={()=> onDelete(comment._id)}
+        className='text-gray-400 hover:text-red-500'>
+            Delete
+        </button>
+      </>
       )
      }
         </div> </>
